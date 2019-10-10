b8fa46f5b9a23591f73c5b4d228e29c6a2b354a0
Smita <Smita@SMITA>
2019-10-10 16:00:09 +0530

    added splashscreen

---
+++ app/src/main/java/com/ashwani/sarvpriyefoundation/Main2Activity.kt
@@ -0,0 +1,50 @@
+package com.ashwani.sarvpriyefoundation
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+
+class Main2Activity : AppCompatActivity() {
+
+    internal var pStatus = 0
+    private val handler = Handler()
+    internal lateinit var tv: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        val drawable = ContextCompat.getDrawable(this, R.drawable.circular)
+        val mProgress = findViewById<View>(R.id.circularProgressbar) as ProgressBar
+        mProgress.progress = 0   // Main Progress
+        mProgress.secondaryProgress = 100 // Secondary Progress
+        mProgress.max = 100 // Maximum Progress
+        mProgress.progressDrawable = drawable
+
+
+        Thread(Runnable {
+            while (pStatus < 100) {
+                pStatus += 1
+
+                handler.post {
+                    mProgress.progress = pStatus
+                    if (mProgress.progress == mProgress.max)
+                        startActivity(Intent(this@Main2Activity, MainActivity::class.java))
+
+
+                }
+                try {
+                    Thread.sleep(16)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+
+            }
+        }).start()
+
+    }
+}
